896560647ff8a73ee842829bb81b1e17761b1614
P H <p.houska@cgv.tugraz.at>
2024-05-25 13:31:19 +0200

    added very simple script to convert .obj files to .bin
    misc: updated url to tools bgfx-doc webpage

---
+++ convert-objs.bat
@@ -0,0 +1,3 @@
+@REM https://bkaradzic.github.io/bgfx/tools.html
+
+.\.build\win64_vs2022\bin\geometrycRelease.exe -f .\examples\assets\meshes\Primitive_Doorway.obj -o .\examples\runtime\meshes\Primitive_Doorway.bin
